45c6fbc5ee727f10ee4dad6ec70dbd91774f9434
Your Name <you@example.com>
2022-01-27 17:22:56 +0800

    fix Linux build

--- src/ssim-cli.cpp
+++ src/ssim-cli.cpp
@@ -34,7 +34,11 @@
     #define _T(s)     s
     #define _tmain    main
     #define _tcscmp   strcmp
-    #define _fprintf  fprintf
+    #define _ftprintf  fprintf
+    #define _tfopen fopen
+    #define _tcschr strchr
+    #define _tcsicmp strcasecmp
+    #define _tcsrchr strrchr
 #endif
 
 
